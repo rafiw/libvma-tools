5768a27a85c551e19912e7d523ac530f7cdfee5e
rafi wiener <rafiwiener@gmail.com>
2017-06-26 18:23:48 +0300

    compile with O3 flag

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-g++ -O0 -m64 -g -Wall -D_FILE_OFFSET_BITS=64 streamCaptureDemo.cpp -o streamCaptureDemo   -I$1/src/vma -L. -lpthread -lrt  -ldl
+g++ -O3 -m64 -g -Wall -D_FILE_OFFSET_BITS=64 streamCaptureDemo.cpp -o streamCaptureDemo   -I$1/src/vma -L. -lpthread -lrt  -ldl
